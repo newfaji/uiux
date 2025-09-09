9f9a586bc48da041c480ad8cf1568e0db3987bbf
newfaji <158270331+newfaji@users.noreply.github.com>
2025-09-09 18:06:51 +0900

    .

---
+++ test/js/js_250909.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	
+    console.log('1111111')
+
+    $('.box').on('mouseenter', function(){
+        console.log('오버했다!!!!')
+        $('.box').addClass('on')
+    })
+    $('.box').on('mouseleave', function(){
+        console.log('내려갔다!')
+        $('.box').removeClass('on')
+    })
+
+})//$(document).ready
+
+console.log('2연결되었습니다.')
