be7751bac873865913f746cccdbb402b8db97a8a
newfaji <158270331+newfaji@users.noreply.github.com>
2025-09-25 18:34:17 +0900

    0

--- eulyoo/js/main.js
+++ eulyoo/js/main.js
@@ -60,4 +60,20 @@ $(document).ready(function(){
         scroll_chk()
     })
 
+    //book swiper 
+    const book_swiper = new Swiper('.book .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            769: {    /* 640px 이상일때 적용 */
+                slidesPerView: 'auto',    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 24,
+            },
+        },
+        navigation: {
+            nextEl: '.book .btn_next',
+            prevEl: '.book .btn_prev',
+        },
+    });//swiper 종료
+
 })//맨끝
