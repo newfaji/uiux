d9d2dd853aa4e38620ac74e05584fe51c977b89e
newfaji <158270331+newfaji@users.noreply.github.com>
2025-10-16 17:59:54 +0900

    .

--- animal/js/main.js
+++ animal/js/main.js
@@ -1,4 +1,27 @@
 $(document).ready(function(){
+
+    /************************ 시작 :: 지금 pc버전이지 모바일인지 체크 (메뉴상태)  ***************/
+
+    let mobile_size = 1024
+    let window_w 
+    let device_status // pc, mobile 
+
+    function device_chk(){ //함수를 정의한다 (선언)
+        window_w = $(window).width()
+        if(window_w > mobile_size){ //브라우저 넓이가 1024보다 클때
+            device_status = 'pc'
+        }else{
+            device_status = 'mobile'
+        }
+        console.log(device_status)
+    }
+
+    device_chk() //html의 로딩이 완료된 이후 단 1번 실행
+    $(window).resize(function(){ //브라우저가 리사이즈 될때마다 실행
+        device_chk() 
+    })
+
+    /************************ 시작 :: 지금 pc버전이지 모바일인지 체크 (메뉴상태)  ***************/
     
     /************************ 시작 :: visual swiper  ***************/
     let visual_time = 5000
@@ -49,5 +72,37 @@ $(document).ready(function(){
         updateCurrent();
     });
     /************************ 끝 :: visual swiper ***************/
+
+
+    /************************ 시작 : pc버전 메뉴 오버 **************
+     * 메뉴에 마우스를 오버했을때  (header .gnb)
+     * header에 menu_pc 클래스를 추가
+     * 마우스를 오버한 메뉴의 1차 메뉴 li에 over를 클래스 추가 (header .gnb .gnb_wrap ul.depth1 > li)
+     * --> 오버한 li에만 over 클래스 줌
+     * ===> 모든 li에서 over를 빼고 오버한 li에만 over클래스 줌
+     * pc버전에서만 .... 
+     * 메뉴를 오버해서 바뀐 색상의 영역 내부에서는 오버가 유지되고 그 밖에 나갈때 아웃
+    */
+
+    $('header .gnb .gnb_wrap ul.depth1 > li').on('mouseenter focusin', function(){
+        if(device_status == 'pc'){ //pc일때만 동작
+            // console.log('오버했음')
+            $('header').addClass('menu_pc')
+            $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('over')
+            $(this).addClass('over')
+        }
+    })
+    $('header .gnb .gnb_wrap ul.depth1 > li').on('mouseleave', function(){
+        $(this).removeClass('over')
+    })
+    $('header').on('mouseleave', function(){
+        $(this).removeClass('menu_pc')
+    })
+
+    $('header .util .search .sch_open').on('focusin', function(){
+        $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('over')
+    })
+
+    /************************ 끝 : pc버전 메뉴 오버 ***************/
 
 })//맨끝
