13464c003f3417c48e896184e6886b099c72114b
newfaji <158270331+newfaji@users.noreply.github.com>
2025-11-07 17:58:37 +0900

    .

--- forest/js/main.js
+++ forest/js/main.js
@@ -30,7 +30,7 @@ $(document).ready(function(){
 		},
 
 		responsiveWidth: 769, /* fullpage를 적용시키지 않을 모바일 사이즈 */
-        responsiveHeight: 500
+        responsiveHeight: 600
 	});//fullpage
 
 	let visual_name = ['서울마이트리', '2025 후원의달', '고목나무이야기', '후원자인터뷰']
@@ -70,6 +70,17 @@ $(document).ready(function(){
 	})
 
 
-	
+	const news_swiper = new Swiper('.news .swiper', { /* 팝업을 감싼는 요소의 class명 */
+		slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+		spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+		breakpoints: {
+			1025: {    /* 640px 이상일때 적용 */
+				slidesPerView: 4,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+				spaceBetween: 24,
+			},
+		},
+		//centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+		//loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+	});
 
 })//맨끝
